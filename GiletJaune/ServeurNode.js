d19db630102064aff3ef3cbfd19f4b9420ffa0f3
Fabloking <fabien.rosse@viacesi.fr>
2019-10-19 15:32:10 +0200

    requête Inscription API

--- GiletJaune/ServeurNode.js
+++ GiletJaune/ServeurNode.js
@@ -8,7 +8,7 @@ var mongoose = require('mongoose');
 var options = { server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } }, 
 replset: { socketOptions: { keepAlive: 300000, connectTimeoutMS : 30000 } } };
 
-var urlmongo = "mongodb://Fabien:1234/DBGiletJaune"; 
+var urlmongo = "mongodb://localhost/DBGiletJaune"; 
 
 mongoose.connect(urlmongo, options);
 
@@ -23,19 +23,47 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 var utilisateurSchema = mongoose.Schema({
-    nom: String, 
-    adresse: String, 
-    tel: String, 
-    description: String   
+	nom: String,
+	prenom: String, 
+    mail: String, 
+    mdp: String, 
+	ddn: String,
+	role: String   
 }); 
 
-var Utilisateur = mongoose.model('utilisateur', utilisateurSchema); 
-var myRouter = express.Router(); 
+var Utilisateur = mongoose.model('utilisateur', utilisateurSchema);
+
+var myRouter = express.Router();
+
 myRouter.route('/')
 .all(function(req,res){ 
       res.json({message : "Bienvenue chez les Gilets Jaune", methode : req.method});
 });
   
+myRouter.route('/inscription')
+.post(function(req,res){
+	var utilisateur = new Utilisateur();
+
+	if(req.body.user_mdp = req.body.user_mdp2){
+
+		utilisateur.nom = req.body.user_nom;
+		utilisateur.prenom = req.body.user_prenom;
+		utilisateur.mail = req.body.user_email;
+		utilisateur.mdp = req.body.user_mdp;
+		utilisateur.ddn = req.body.user_ddn;
+		utilisateur.role = req.body.role;
+		
+		utilisateur.save(function(err){
+			if(err){
+			  res.send(err);
+			}
+			res.sendFile('/src/app/Composants/time-line/time-line.component.html',{ root : __dirname});
+		  })
+	} else {
+		res.send({message : "Le mot de passe ne correspond pas !"})
+	}
+
+});
 
 app.use(myRouter);   
 app.listen(port, hostname, function(){
